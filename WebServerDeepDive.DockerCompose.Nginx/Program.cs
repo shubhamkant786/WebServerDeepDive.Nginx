b2eeaa40b8a034ea3719f88a240fc7f5e755861f
Shubham Kant Awasthi <shubhamkant.awasthi@belgiantrain.be>
2026-09-08 16:51:49 +0200

    Add project files.

---
+++ WebServerDeepDive.DockerCompose.Nginx/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Define the availability on all IPs with the defined port
+//builder.WebHost.UseUrls("http://*:5055");
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseForwardedHeaders();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
